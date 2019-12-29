c58ef6649374838dc34a515c740046559819fb15
Jonathan Brooks <brooks.jonathan1@gmail.com>
2019-12-29 17:00:50 +0100

    Created react app, and started on making the website

---
+++ jonathanbrooks/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TBD from './components/TBD'
+
+function App() {
+  return (
+    <div className="TBD">
+      <TBD/>
+    </div>
+  );
+}
+
+export default App;
---
+++ jonathanbrooks/src/components/TBD.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TBDcss.css';
+
+function TBD() {
+    return (
+        <div className="tbd">
+            <h1>Snart ferdig™</h1>
+        </div>
+    )
+}
+
+export default TBD;
